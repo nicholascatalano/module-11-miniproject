f85ecf1fcddfaab290531b3e52c9518b12aa7de8
nickcatalano5 <nickcatalano24@gmail.com>
2023-12-22 12:01:13 -0500

    adjusted process env variable

--- routes/diagnostics.js
+++ routes/diagnostics.js
@@ -3,32 +3,32 @@ const { v4: uuidv4 } = require("uuid");
 const { readAndAppend, readFromFile } = require("../helpers/fsUtils");
 
 // GET Route for retrieving diagnostic information
-// diagnostics.get("/", (req, res) => {
-//   // TODO: Logic for sending all the content of db/diagnostics.json
-//   readFromFile(`./db/diagnostics.json`).then((data) =>
-//     res.json(JSON.parse(data))
-//   );
-// });
+diagnostics.get("/", (req, res) => {
+  // TODO: Logic for sending all the content of db/diagnostics.json
+  readFromFile(`./db/diagnostics.json`).then((data) =>
+    res.json(JSON.parse(data))
+  );
+});
 
 // POST Route for a error logging
-// diagnostics.post("/", (req, res) => {
-//   // TODO: Logic for appending data to the db/diagnostics.json file
-//   console.log(req.body);
+diagnostics.post("/", (req, res) => {
+  // TODO: Logic for appending data to the db/diagnostics.json file
+  console.log(req.body);
 
-//   const { ...errors } = req.body;
+  const { ...errors } = req.body;
 
-//   if (req.body) {
-//     const newDiagnostic = {
-//       time,
-//       error_id: uuidv4(),
-//       ...errors,
-//     };
+  if (req.body) {
+    const newDiagnostic = {
+      time,
+      error_id: uuidv4(),
+      ...errors,
+    };
 
-//     readAndAppend(newDiagnostic, `./db/diagnostics.json`);
-//     res.json(`Diagnostic report added successfully.`);
-//   } else {
-//     res.error(`Error in adding diagnostic report.`);
-//   }
-// });
+    readAndAppend(newDiagnostic, `./db/diagnostics.json`);
+    res.json(`Diagnostic report added successfully.`);
+  } else {
+    res.error(`Error in adding diagnostic report.`);
+  }
+});
 
 module.exports = diagnostics;
